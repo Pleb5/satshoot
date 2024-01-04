f6cc7b254c11802130c7525853f448a8758ca86d
Five <co22rkn@protonmail.com>
2024-01-04 16:49:31 +0100

    Basic Login UI created with some login buttons for different ways to login

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,28 @@
+<h1 class="h1 text-center my-8">Bitcoin Troubleshoot Login</h1>
+
+<div class="flex items-center ">
+    <div class="flex flex-col items-start gap-y-5 mx-auto">
+        <div class="flex gap-x-4">
+            <button class="btn btn-lg bg-primary-300-600-token " >
+                <span>Browser Extension </span>
+            </button>
+
+            <a 
+                class="btn btn-md bg-warning-300-600-token"
+                href="https://getalby.com/"
+                target="_blank"
+            >
+                <span>See Alby</span>
+            </a>
+        </div>
+
+        <button class="btn btn-lg bg-primary-300-600-token " >
+            <span>Generate ephemeral Nostr identity</span>
+        </button>
+
+        <button class="btn btn-lg bg-primary-300-600-token " >
+            <span>Login with nsecBunker</span>
+        </button>
+    </div>
+
+</div>
