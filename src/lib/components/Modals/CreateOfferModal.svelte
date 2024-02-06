7e08bdd984d1d4a31d9c5414a16c488530ad72e2
Five <co22rkn@protonmail.com>
2024-02-06 17:09:45 +0100

    Ticket page and Create Offer: basics work, still much testing needed - Refreshing page creates error state - author in Offer event is undefined while activeUser is defined before event publish - Should create a Toast message about Relays that refused the event publish - Offers on ticket count doesnt work yet - Pricing and amount tags get duplicated from multiple publishes
    Need to look into TicketEvent and OfferEvent classes for details on these bugs

---
+++ src/lib/components/Modals/CreateOfferModal.svelte
@@ -0,0 +1,128 @@
+<script lang="ts">
+	import type { SvelteComponent } from 'svelte';
+    import ndk from '$lib/stores/ndk';
+    import { NDKRelaySet } from '@nostr-dev-kit/ndk';
+    import type { NDKUser } from '@nostr-dev-kit/ndk';
+    import { OfferEvent, Pricing } from '$lib/events/OfferEvent';
+    
+	// Stores
+	import { getModalStore } from '@skeletonlabs/skeleton';
+    import type { ToastSettings } from '@skeletonlabs/skeleton';
+    import { getToastStore } from '@skeletonlabs/skeleton';
+
+	// Props
+	/** Exposes parent props to this component. */
+	export let parent: SvelteComponent;
+
+    export let ticketAddress: string | undefined = undefined;
+
+	const modalStore = getModalStore();
+    const toastStore = getToastStore();
+
+    let pricingMethod: Pricing;
+
+    let amount: number = 0;
+
+    let description: string = '';
+
+    let errorText = '';
+    
+    const cBase = 'card bg-surface-100-800-token w-screen/2 h-screen/2 p-8 flex justify-center items-center';
+
+    async function postOffer() {
+        const offer = new OfferEvent($ndk)
+
+        console.log($ndk.activeUser)
+        offer.author = $ndk.activeUser as NDKUser;
+
+        offer.pricing = pricingMethod;
+        offer.amount = amount;
+        offer.description = description;
+
+        offer.referencedTicketAddress = ticketAddress as string;
+
+        // generate unique d-tag
+        offer.generateTags();
+
+        console.log(offer)
+
+        try {
+
+            const relaysPublished = await offer.publish(
+                new NDKRelaySet(new Set($ndk.pool.relays.values()), $ndk)
+            );
+
+            console.log(relaysPublished)
+
+            const t: ToastSettings = {
+                message: 'Offer Posted!',
+                timeout: 7000,
+                background: 'bg-success-300-600-token',
+            };
+            toastStore.trigger(t);
+
+            modalStore.close();
+        } catch(e) {
+            errorText = 'Error happened while publishing Offer!';
+        }
+    }
+
+</script>
+
+{#if $modalStore[0]}
+<div class="{cBase}">
+    {#if ticketAddress}
+            <div class="grid grid-cols-1">
+                <h2 class="h2 text-center">Create Offer</h2>
+                <form on:submit|preventDefault={ postOffer }>
+                        <!-- Pricing -->
+                    <label class="m-4">
+                        <span>Pricing Method</span>
+                        <select class="select" bind:value={pricingMethod}>
+                            <option value={Pricing.SatsPerMin}>Sats/Minute</option>
+                            <option value={Pricing.Absolute}>Absolute Price (sats)</option>
+                        </select>
+                    </label>
+                    <!-- Amount -->
+                    <label class="m-4">
+                        <span>Amount</span>
+                        <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+                            <div class="input-group-shim">
+                                <i class="fa-brands fa-bitcoin text-3xl"/>
+                            </div>
+                            <input class="text-lg max-w-md" type="number" placeholder="Amount" bind:value={amount} />
+                            <div>{pricingMethod ? 'sats' : 'sats/min'}</div>
+                        </div>
+                    </label>
+                    <!-- Description -->
+                    <label class="label max-w-xl m-4">
+                        <span>Offer Description</span>
+                        <textarea 
+                            class="textarea"
+                            rows="4"
+                            placeholder="Describe how you would solve the issue and why you should get the job"
+                            bind:value={description}
+                        />
+                    </label>
+                    <div class="flex justify-center mb-8">
+                        <button 
+                            type="submit"
+                            class="btn font-bold bg-success-400-500-token w-72 mt-4" 
+                        >
+                            Post Offer
+                        </button>
+                    </div>
+                </form>
+            </div>
+        {#if errorText}
+            <h2 class="h2 font-bold text-center text-error-300-600-token">
+                {errorText}
+            </h2>
+        {/if}
+    {:else}
+        <h2 class="h2 font-bold text-center text-error-300-600-token">
+            Error: Ticket is missing!
+        </h2>
+    {/if}
+</div>
+{/if}
--- src/routes/[ticketId=event]/+page.svelte
+++ src/routes/[ticketId=event]/+page.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
     import type { TicketEvent } from "$lib/events/TicketEvent";
+    import type { OfferEvent } from "$lib/events/OfferEvent";
     import { tickets, offers } from "$lib/stores/troubleshoot-eventstores";
     import ndk from "$lib/stores/ndk";
 
+    import CreateOfferModal from "$lib/components/Modals/CreateOfferModal.svelte";
     import { getModalStore } from "@skeletonlabs/skeleton";
+    import type { ModalComponent, ModalSettings } from "@skeletonlabs/skeleton";
+
+    import { popup } from '@skeletonlabs/skeleton';
+    import type { PopupSettings } from '@skeletonlabs/skeleton';
+
     import { page } from '$app/stores';
     import UserCard from "$lib/components/User/UserCard.svelte";
     
@@ -14,17 +21,50 @@
     $tickets.forEach((ticketEvent) => {
         if (ticketEvent.encode() === $page.params.ticketId) {
             ticket = ticketEvent;
+            console.log(ticket)
         }
     });
 
-    console.log(ticket)
+    let offersForTicket: OfferEvent[] = [];
+
+    console.log('offers: ', $offers)
+
+    $: {
+        $offers.forEach((offer: OfferEvent) =>{
+            console.log('in offer forEach!')
+            console.log(offer)
+            if(offer.offerAddress === ticket?.acceptedOfferAddress) {
+                offersForTicket.push(offer)
+                console.log('offer found on ticket!')
+            }
+        })
+    }
+
+    function createOffer() {
+
+        const modalComponent: ModalComponent = {
+            ref: CreateOfferModal,
+            props: {ticketAddress: ticket?.ticketAddress},
+        };
+
+        const modal: ModalSettings = {
+            type: 'component',
+            component: modalComponent,
+        };
+        modalStore.trigger(modal);
+    }
 
 
-
+    // For tootip    
+    const popupHover: PopupSettings = {
+        event: 'click',
+        target: 'popupHover',
+        placement: 'right'
+    };
     
 </script>
 
-<div class="flex flex-col justify-center gap-y-4 mt-4" >
+<div class="flex flex-col justify-center gap-y-4 mt-4 p-8" >
     {#if ticket}
         <h2 class="text-center font-bold" >Ticket:</h2>
         <div class="card">
@@ -35,16 +75,46 @@
 
             <section class="p-4 text-lg">{ticket.description ?? 'No description!'}</section>
 
-            <footer class="items-center flex flex-wrap card-footer">
-                {#each ticket.tTags as tag }
-                    <div class="px-2 rounded-token">
-                        <span class="text-sm badge variant-filled-surface">{ tag[1] }</span>
+            <footer class="grid grid-cols-5 card-footer">
+                <div class="grid col-span-4">
+                    <div class="items-center flex flex-wrap">
+                        {#each ticket.tTags as tag }
+                            <div class="px-2 rounded-token">
+                                <span class="text-sm badge variant-filled-surface">{ tag[1] }</span>
+                            </div>
+                        {/each}
                     </div>
-                {/each}
+                </div>
+                <div class="text-sm font-bold">{'Offers on ticket: ' + offersForTicket?.length}</div>
             </footer>
 
         </div>
-            <!-- User -->
+                    <!-- Submit -->
+        <div class="flex justify-center items-center gap-x-2">
+            <button 
+                type="button"
+                on:click={ createOffer }
+                class="btn btn-2xl text-xl font-bold bg-primary-300-600-token"
+                disabled={!$ndk.activeUser}
+            >
+                Create Offer
+            </button>
+            {#if !$ndk.activeUser}
+                <i 
+                    class="text-primary-300-600-token fa-solid fa-circle-question text-2xl
+                    [&>*]:pointer-events-none" 
+                    use:popup={popupHover}
+                />
+
+                <div class="card w-80 p-4 bg-primary-300-600-token" data-popup="popupHover">
+                    <p>
+                        Need to log in before creating an offer!
+                    </p>
+                    <div class="arrow bg-primary-300-600-token" />
+                </div>
+            {/if}
+        </div>
+                    <!-- User -->
         <h2 class="text-center font-bold" >Posted by:</h2>
         <UserCard ndk={$ndk} npub={ticket.author.npub} />
     {:else}
