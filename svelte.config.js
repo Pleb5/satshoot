97b581ca65c477df6b401b9d92d353b9e3b3b5cf
Five <co22rkn@protonmail.com>
2024-03-06 16:25:51 +0100

    Preparing first deploy: Updated most dependencies(ndk-related, sveltekit, vite) - Also added some configs for building static site - Added client side ndk caching package 'ndk-cache-dexie' necessary for Notifications feature - Also added service worker for PWA Notifications (will be implemented later on with Dexie)

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+// -------------For build----------------------
+// import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,11 +10,19 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+        adapter: adapter(
+            // ---------------- For build ------------------
+            // {
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            // pages: 'build',
+            // assets: 'build',
+            // fallback: undefined,
+            // precompress: false,
+            // strict: true
+            // }
+        )
+    }
 };
 
 export default config;
