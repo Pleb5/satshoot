42d439280cbaab02b4b1e3226c790247fe1fd3e0
Five <co22rkn@protonmail.com>
2024-03-06 04:42:31 +0100

    Settings Menu: used the recommended way to create a nested div for tailwind styling on popup element instead of adding classes to top element data-popup

--- src/lib/components/PopupMenu/SettingsMenu.svelte
+++ src/lib/components/PopupMenu/SettingsMenu.svelte
@@ -103,50 +103,52 @@
     }
 </script>
 
-<div data-popup="settingsMenu" class="card p-4 w-60 shadow-xl ">
-    <nav class="list-nav">
-        <ul>
-            <li>
-                <a href={ "/" + $ndk.activeUser?.npub }>
-                    <span class="w-6 text-center"><i class="fa-solid fa-user" /></span>
-                    <span>Profile</span>
-                </a>
-            </li>
-            <li>
-                <a href="/network">
-                    <span class="w-6 text-center"><i class="fa-solid fa-globe" /></span>
-                    <span>Network</span>
-                </a>
-            </li>
-            <hr class="!my-4" />
-            <li>
-                <a href="/">
-                    <span class="w-6 text-center"><i class="fa-solid fa-question" /></span>
-                    <span>FAQ</span>
-                </a>
-            </li>
-            <li>
-                <a href="/">
-                    <span class="w-6 text-center"><i class="fa-solid fa-info" /></span>
-                    <span>About</span>
-                </a>
-            </li>
-            <li>
-                <a href="/">
-                    <span class="w-6 text-center"><i class="fa-regular fa-comment" /></span>
-                    <span>Feedback</span>
-                </a>
-            </li>
-            <hr class="!my-4" />
-            <li>
-                <button class="w-full" on:click={logout}>
-                    <span class="w-6 text-center">
-                        <i class="fa-solid fa-arrow-right-from-bracket" />
-                    </span>
-                    <span>Logout</span>
-                </button>
-            </li>
-        </ul>
-    </nav>
+<div data-popup="settingsMenu" >
+    <div class="card p-4 w-60 shadow-xl z-50">
+        <nav class="list-nav">
+            <ul>
+                <li>
+                    <a href={ "/" + $ndk.activeUser?.npub }>
+                        <span class="w-6 text-center"><i class="fa-solid fa-user" /></span>
+                        <span>Profile</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/network">
+                        <span class="w-6 text-center"><i class="fa-solid fa-globe" /></span>
+                        <span>Network</span>
+                    </a>
+                </li>
+                <hr class="!my-4" />
+                <li>
+                    <a href="/">
+                        <span class="w-6 text-center"><i class="fa-solid fa-question" /></span>
+                        <span>FAQ</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                        <span class="w-6 text-center"><i class="fa-solid fa-info" /></span>
+                        <span>About</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                        <span class="w-6 text-center"><i class="fa-regular fa-comment" /></span>
+                        <span>Feedback</span>
+                    </a>
+                </li>
+                <hr class="!my-4" />
+                <li>
+                    <button class="w-full" on:click={logout}>
+                        <span class="w-6 text-center">
+                            <i class="fa-solid fa-arrow-right-from-bracket" />
+                        </span>
+                        <span>Logout</span>
+                    </button>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </div>
 
