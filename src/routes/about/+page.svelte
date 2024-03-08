4b422ccc5294bd07106732c724e0cdcdd2bd4010
Five <co22rkn@protonmail.com>
2024-03-08 09:58:27 +0100

    Created an initial about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,84 +1,54 @@
 <script lang="ts">
-    // Import the package
-    import NDK, { NDKEvent } from "@nostr-dev-kit/ndk";
-    import { browser } from '$app/environment';
-    import {NDKNip07Signer} from "@nostr-dev-kit/ndk";
-
-
-    const signer = new NDKNip07Signer();
-
-    // Create a new NDK instance with explicit relays
-    const ndk = new NDK({
-        // signer: signer,
-        explicitRelayUrls: ["wss://relay.damus.io", "wss://relay.snort.social"],
-    });
-
-    if (browser) {
-        ndk.connect().then(() => {
-            console.log('Connected');
-        });
-    }
-
-    const user = ndk.getUser({
-        npub: 'npub16p8v7varqwjes5hak6q7mz6pygqm4pwc6gve4mrned3xs8tz42gq7kfhdw'
-    });
-
-    console.log(user);
-
-    // const eventpromise = ndk.fetchEvents({ kinds: [1], authors: [user.pubkey] });
-    //
-    async function login () {
-       signer.user().then(async (user) => {
-            if (!!user.npub) {
-                console.log("Permission granted to read user public key: ", user.npub);
-            }
-        });
-    }
-
-    function publishTroubleshootEvent() {
-        const troubleshootEvent = new NDKEvent(ndk);
-        
-        troubleshootEvent.kind = 32767;
-        troubleshootEvent.content = "SOS help me I have to withdraw my funds from the exchange!";
-
-        troubleshootEvent.publish();
-    }
-
-
+    import pageTitleStore from "$lib/stores/pagetitle-store";
+    $pageTitleStore = 'About';
 </script>
 
-<h1> Bitcoin troubleshoot </h1>
+<h2 class="h2 text-center">Bitcoin Troubleshoot</h2>
+<div class="card mx-10 mt-4 p-2">
+    <h4 class="h4 text-center">What is Bitcoin Troubleshoot?</h4>
+    <p>
+        Bitcoin Troubleshoot is an app for bitcoiners to help each other in Self Custody
+        issues with free market incentives. It is essentially P2P market with an auction mechanism.
+    </p>
 
-<button on:click={login}>Log in with NIP07</button>
+</div>
+<div class="card mx-10 mt-4 p-2">
+    <h4 class="h4 text-center">So how does this work in action?</h4>
+    <p>
+        Let's take the example of Alice who is a bitcoiner and has a problem with her node.
+        She cannot connect to it from her Software Wallet. She creates a 'Ticket' in 
+        Bitcoin Troubleshoot and posts it on nostr. Now anyone who has a clue how to solve her 
+        problem can bid on it with an 'Offer'. This Offer contains the price of the Troubleshooting.
+    </p>
+
+    <p class="mt-2">
+        Bob who is deeper down the rabbit hole finds this ticket in the feed and bids on it with an offer. 
+        Alice received a bunch of offers from different bitcoiners but she trusts Bob the most and finds his 
+        price affordable. She accepts Bob's Offer. They can now resolve the node issue via encrypted chat or 
+        they can also just switch to a different platform for the actual Troubleshooting part.
+    </p>
+
+    <p class="mt-2">
+        Bob successfully helped Alice with her problem. He can now receive the payment they agreed on. This can 
+        happen on nostr or some other way. Most likely they will pay the bitcoin price through the Lightning Network 
+        which is private and cheap. 
+    </p>
+</div>
 
 
-<button on:click={publishTroubleshootEvent}>Ask for help</button>
-
-<!-- {#await user.fetchProfile() then events} -->
-<!--     <h2>{user.profile?.name}</h2> -->
-<!--     <p> -->
-<!--         <img src={user.profile?.image} style="width:100px; height:100px;" alt="five_npub" /> -->
-<!--     </p> -->
-<!--     <p>{user.profile?.about}</p> -->
-<!-- {/await} -->
-
-
-
-
-<!-- {#await eventpromise then events} -->
-<!--     {#each Array.from(events) as event} -->
-<!--         <div class=eventBlock> -->
-<!--             <p>{event.content}</p> -->
-<!---->
-<!--         </div> -->
-<!--     {/each} -->
-<!-- {/await} -->
-<!---->
-<!-- <style> -->
-<!--     .eventBlock { -->
-<!--         padding:10px; -->
-<!--         border:1px black; -->
-<!--         border-radius:10px; -->
-<!--         margin-bottom: 4px; -->
-<!--     } -->
-<!-- </style> -->
+<div class="card mx-10 mt-4 p-2">
+    <h4 class="h4 text-center">How is this better than other solutions?</h4>
+    <p>
+        There are many options for bitcoiners to find technical help. I believe however, 
+        that a dedicated Nostr solution is the best way to go for several reasons:
+    </p>
+    <ul>
+        <li>- The nostr ecosystem is already full of bitcoiners eager to help each other out</li>
+        <li class="mt-1">- With Nostr you don't tie yourself to a particular individual or company. Anyone can help anyone, any time, from anywhere, without permission</li>
+        <li class="mt-1">- You can use your social graph on Nostr to find trustworthy clients / troubleshooters</li>
+        <li class="mt-1">- Nostr is an excellent protocol to run Peer-to-Peer Markets. Easy to switch Relays and anyone can easily build a new client</li>
+        <li class="mt-1">- Nostr allows for such a symbiosis between apps that a legacy solution will never be able to provide</li>
+        <li class="mt-1">- Nostr found a nice tradeoff-balance where reliable services can be built in a less centralized way, empowering end-users and fostering open competition as a result</li>
+        <li class="mt-1">- With Nostr, You hold your keys to Your online identity and data, You are in control</li>
+    </ul>
+</div>
