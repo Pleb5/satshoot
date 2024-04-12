0ae68792fc71ba78051046bff0cb7e8cd38006b1
Five <co22rkn@protonmail.com>
2024-04-12 10:13:25 +0200

    Ticket page Routing: Only allow naddr1 encoded nip19 tickets(parameterized replaceable events)

--- src/params/event.ts
+++ src/params/event.ts
@@ -1,5 +1,5 @@
 /** @type {import('@sveltejs/kit').ParamMatcher} */
 
 export function match(param:string) {
-    return /^(nevent1|note1|naddr1)[a-zA-Z0-9]*/.test(param);
+    return /^(naddr1)[a-zA-Z0-9]*/.test(param);
 }
