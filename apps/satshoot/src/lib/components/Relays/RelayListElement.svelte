6e943c1af6f6f46ea17d69d55283bac86dbc8d44
Five <co22rkn@protonmail.com>
2024-06-11 12:33:07 +0000

    Network page: Reworked to be able to handle Outbox read/write relays - Can edit either types of relays - Broadcasts to blastr(wss://nostr.mutiny.com), ndk pool and outbox pool - Still the relay connectivity is very bad, reason unkown, need to investigate

--- apps/satshoot/src/lib/components/Relays/RelayListElement.svelte
+++ apps/satshoot/src/lib/components/Relays/RelayListElement.svelte
@@ -6,25 +6,31 @@
     let relayStatusColor: string;
     let relayStatusText: string;
 
-    console.log('relay in relay listelement', relay)
-    setTimeout(()=>{
-        console.log('relay in relay listelement after delay', relay)
-    }, 8000);            
-    $: {
-        if (relay) {
-            if (relay.status == NDKRelayStatus.CONNECTING) {
-                relayStatusColor = "variant-filled-warning";
-                relayStatusText = "Connecting";
-            } else if (relay.status == NDKRelayStatus.DISCONNECTED) {
-                relayStatusColor = "variant-filled-error";
-                relayStatusText = "Disconnected";
-            } else if (relay.status == NDKRelayStatus.CONNECTED) {
-                relayStatusColor = "variant-filled-success";
-                relayStatusText = "Connected";
-            } else if (relay.status == NDKRelayStatus.FLAPPING) {
-                relayStatusColor = "variant-filled-warning";
-                relayStatusText = "Flapping";
-            }
+    // console.log('relay in relay listelement', relay)
+    // setTimeout(()=>{
+    //     console.log('relay in relay listelement after delay', relay)
+    // }, 8000);            
+
+    $: if (relay && relay.status) {
+        console.log('relay status changed: ', relay.status)
+        if (
+            relay.status == NDKRelayStatus.CONNECTING
+                || relay.status === NDKRelayStatus.RECONNECTING
+        ) {
+            relayStatusColor = "variant-filled-warning";
+            relayStatusText = "Connecting";
+        } else if (relay.status == NDKRelayStatus.DISCONNECTED) {
+            relayStatusColor = "variant-filled-error";
+            relayStatusText = "Disconnected";
+        } else if (relay.status == NDKRelayStatus.CONNECTED) {
+            relayStatusColor = "variant-filled-success";
+            relayStatusText = "Connected";
+        } else if (relay.status == NDKRelayStatus.FLAPPING) {
+            relayStatusColor = "variant-filled-warning";
+            relayStatusText = "Flapping";
+        } else if (relay.status === NDKRelayStatus.AUTHENTICATING) {
+            relayStatusColor = "variant-filled-primary";
+            relayStatusText = "Authenticating";
         }
     }
 
--- apps/satshoot/src/routes/network/+page.svelte
+++ apps/satshoot/src/routes/network/+page.svelte
@@ -5,45 +5,35 @@
 
     import normalizeUrl from "normalize-url";
 
-    import { NDKRelay, NDKRelayList, NDKSubscriptionCacheUsage } from '@nostr-dev-kit/ndk';
+    import {
+        NDKRelay,
+        NDKRelayList,
+        NDKSubscriptionCacheUsage,
+        NDKRelaySet
+    } from '@nostr-dev-kit/ndk';
     import RelayListElement from '$lib/components/Relays/RelayListElement.svelte';
     import { getModalStore, type ModalSettings } from '@skeletonlabs/skeleton';
+    import { onMount, tick } from 'svelte';
+
+    import { getToastStore } from '@skeletonlabs/skeleton';
+    import type { ToastSettings } from '@skeletonlabs/skeleton';
+    import { ProgressRadial } from '@skeletonlabs/skeleton';
 
     const modalStore = getModalStore();
+    const toastStore = getToastStore();
 
     let needRelays = true;
+    let posting = false;
 
+    const readRelayUrls: Set<string> = new Set();
+    const writeRelayUrls: Set<string> = new Set();
     let readRelays: Set<NDKRelay> = new Set();
     let writeRelays: Set<NDKRelay> = new Set();
 
     let readRelayInputValue = '';
     let writeRelayInputValue = '';
 
-
-    async function fetchOutboxRelays() {
-        const relays = await $ndk.fetchEvent(
-            { kinds: [10002], authors: [$currentUser!.pubkey] },
-            { 
-                cacheUsage: NDKSubscriptionCacheUsage.ONLY_RELAY,
-                groupable: false,
-            }
-        );
-        console.log('outbox relays', relays)
-
-        const readRelayUrls: Set<string> = new Set();
-        const writeRelayUrls: Set<string> = new Set();
-
-        if (relays) {
-            const relayList = NDKRelayList.from(relays);
-
-            relayList.readRelayUrls.forEach((url: string) => {
-                readRelayUrls.add(url);
-            });
-            relayList.writeRelayUrls.forEach((url: string) => {
-                writeRelayUrls.add(url);
-            });
-        }
-
+    function updateRelayValues() {
         readRelays.clear();
         writeRelays.clear();
         const poolArray = Array.from($ndk.pool.relays.values());
@@ -68,7 +58,32 @@
         console.log('read relays', readRelays)
         console.log('write relays', writeRelays)
     }
-// Url normalization based on the idea of Coracle.social
+
+    async function fetchOutboxRelays() {
+        const relays = await $ndk.fetchEvent(
+            { kinds: [10002], authors: [$currentUser!.pubkey] },
+            { 
+                cacheUsage: NDKSubscriptionCacheUsage.ONLY_RELAY,
+                groupable: false,
+            }
+        );
+        console.log('outbox relays', relays)
+
+
+        if (relays) {
+            const relayList = NDKRelayList.from(relays);
+
+            relayList.readRelayUrls.forEach((url: string) => {
+                readRelayUrls.add(url);
+            });
+            relayList.writeRelayUrls.forEach((url: string) => {
+                writeRelayUrls.add(url);
+            });
+        }
+        updateRelayValues();
+    }
+
+    // Url normalization based on the idea of Coracle.social
     // https://github.com/coracle-social/paravel/blob/7cb792ba17550f208d3c80773c4822a010139ccb/src/util/nostr.ts#L46
     const stripProto = (url: string) => url.replace(/.*:\/\//, "")
     function normalizeRelayUrl(url: string) {
@@ -85,41 +100,120 @@
         return "wss://" + url;
     }
 
-    function addRelay(read: boolean) {
+    async function addRelay(read: boolean) {
         const url: string = read 
             ? normalizeRelayUrl(readRelayInputValue)
             : normalizeRelayUrl(writeRelayInputValue);
 
-       // TODO: push relay list as 10002 with blastr and reload relays 
+        if (!url) return;
 
+        if (read) {
+            readRelayUrls.add(url);
+        } else {
+            writeRelayUrls.add(url);
+        }
+
+        posting = true;
+        await tick();
+
+        try {
+            await broadcastRelays();
+            posting = false;
+
+            const t: ToastSettings = {
+                message: 'New Relay Config Broadcasted!',
+                timeout: 7000,
+                background: 'bg-success-300-600-token',
+            };
+            toastStore.trigger(t);
+
+            if (read) {
+                readRelayInputValue = '';
+            } else {
+                writeRelayInputValue = '';
+            }
+            fetchOutboxRelays();
+        } catch (e) {
+            posting = false;
+            const t: ToastSettings = {
+                message: 'Could not post Relays: ' + e,
+                timeout: 7000,
+                background: 'bg-error-300-600-token',
+            };
+            toastStore.trigger(t);
+            fetchOutboxRelays();
+        }
     }
 
-    function removeRelay(relay: NDKRelay, read: boolean) {
-        let removeRelayResponse = function(r: boolean){
+    function removeRelay(url: string, read: boolean) {
+        let removeRelayResponse = async function(r: boolean){
             if (r) {
+                if (!url) return;
                 // Removal confirmed
                 if (read) {
-
+                    readRelayUrls.delete(url);
                 } else {
-
+                    writeRelayUrls.delete(url);
                 }
-                // TODO: push relay list as 10002 with blastr and user relays then reload relays 
 
+                posting = true;
+                await tick();
+
+                try {
+                    await broadcastRelays();
+
+                    posting = false;
+
+                    const t: ToastSettings = {
+                        message: 'New Relay Config Broadcasted!',
+                        timeout: 7000,
+                        background: 'bg-success-300-600-token',
+                    };
+                    toastStore.trigger(t);
+
+                    fetchOutboxRelays();
+                } catch (e) {
+                    posting = false;
+                    const t: ToastSettings = {
+                        message: 'Could not post Relays: ' + e,
+                        timeout: 7000,
+                        background: 'bg-error-300-600-token',
+                    };
+                    toastStore.trigger(t);
+                    fetchOutboxRelays();
+                }
             }
         }
 
         const modal: ModalSettings = {
             type: 'confirm',
             title: 'Confirm Removal of Relay',
-            body: `Do you really want to remove ${relay.url.replace("wss://","").slice(0, -1)}?`,
+            body: `Do you really want to remove ${url.replace("wss://","").slice(0, -1)}?`,
             response: removeRelayResponse,
         };
         modalStore.trigger(modal);
 
     }
 
+    async function broadcastRelays() {
+        const userRelayList = new NDKRelayList($ndk);
+        userRelayList.writeRelayUrls = Array.from(writeRelayUrls);
+        userRelayList.readRelayUrls = Array.from(readRelayUrls);
+        
+        const blastrUrl = 'wss://nostr.mutinywallet.com';
+        const broadCastRelaySet = NDKRelaySet.fromRelayUrls([
+                blastrUrl,
+                ...$ndk.pool.urls(),
+                ...$ndk.outboxPool!.urls()
+        ], $ndk);
+        console.log('relays sent to:', broadCastRelaySet)
+            
+        const relaysPosted = await userRelayList.publish(broadCastRelaySet);
+        console.log('relays posted to:', relaysPosted)
+    }
+
     $: if ($currentUser && $connected && needRelays) {
-        // needRelays = false;
+        needRelays = false;
         fetchOutboxRelays();
     }
     
@@ -134,61 +228,164 @@
     //         console.log('outbox read relays for user', readRelays)
     //     }
     // }
+
+    onMount(()=>{
+        $ndk.pool.on('connect', () => {
+            updateRelayValues();
+        });
+        $ndk.pool.on('relay:connect', () => {
+            updateRelayValues();
+        });
+        $ndk.pool.on('disconnect', () => {
+            updateRelayValues();
+        });
+    });
 </script>
 
 <h3 class="h3 mb-4 text-center">Your Read Relays(Inbox)</h3>
-<div class="flex flex-col gap-y-4 justify-center items-center mb-6 mx-2">
-    {#each readRelays as relay(relay.url)}
-        <div class="card card-hover grid grid-cols-[1fr_auto] gap-x-2 bg-surface-active-token p-4">
-            <RelayListElement {relay}/>
-            <button 
-                class="btn bg-error-500"
-                type="button" 
-                on:click={()=>{removeRelay(relay, true)}}>
-                Remove
-            </button>
-        </div> 
-    {/each}
-    <div class="w-80 mb-6">
-        <label class="label flex flex-col items-center">
+{#if readRelays && readRelays.size > 0}
+    <div class="flex flex-col gap-y-4 justify-center items-center mb-6 mx-2">
+        {#each readRelays as relay(relay.url)}
+            <div class="card card-hover grid grid-cols-[1fr_auto] gap-x-2 bg-surface-active-token p-4">
+                <RelayListElement {relay}/>
+                <button 
+                    class="btn bg-error-500"
+                    type="button" 
+                    on:click={()=>{removeRelay(relay.url, true)}}>
+                    Remove
+                </button>
+            </div> 
+        {/each}
+        <label class="label flex flex-col items-center mb-6">
             <h3 class="h3">Add READ Relay</h3>
-            <input class="input w-full text-center" 
-            bind:value={readRelayInputValue}
-            title="relay input (url)" 
-            type="text" 
-            placeholder="Enter Relay URL(without wss://)"
-        />
+            <div class="flex gap-x-2">
+                <input class="input w-full text-center" 
+                bind:value={readRelayInputValue}
+                title="relay input (url)" 
+                type="text" 
+                placeholder="Relay URL, without 'wss://'"
+            />
+                <button 
+                    class="btn btn-icon"
+                    type="button" 
+                    on:click={()=>{addRelay(true)}}
+                    disabled={posting}
+                >
+                    {#if posting}
+                        <ProgressRadial
+                        value={undefined}
+                        stroke={60}
+                        meter="stroke-primary-500"
+                        track="stroke-primary-500/30"
+                        strokeLinecap="round" width="w-8" 
+                    />
+                    {:else} 
+                        <span>
+                            <i 
+                                class="text-3xl text-primary-500 fa-solid fa-circle-plus"
+                            >
+                            </i>
+                        </span>
+                    {/if}
+                </button>
+            </div>
         </label>
     </div>
-</div>
+{:else}
+    <div class="p-4 space-y-4">
+        <div class="grid grid-cols-3 gap-8">
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+        </div>
+        <div class="grid grid-cols-4 gap-4">
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+        </div>
+        <div class="grid grid-cols-3 gap-8">
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+        </div>
+    </div>
+{/if}
 <hr class="mb-6"/>
 <h3 class="h3 mb-4 text-center">Your Write Relays(Outbox)</h3>
-<div class="flex flex-col gap-y-4 justify-center items-center mb-6 mx-2">
-    {#each writeRelays as relay(relay.url)}
-        <div class="card card-hover grid grid-cols-[1fr_auto] gap-x-2 bg-surface-active-token p-4">
-            <RelayListElement {relay}/>
-            <button 
-                class="btn bg-error-500"
-                type="button" 
-                on:click={()=>{removeRelay(relay, false)}}>
-                Remove
-            </button>
-        </div> 
-    {/each}
-    <div class="w-80 mb-6">
-        <label class="label flex flex-col items-center">
+{#if writeRelays && writeRelays.size > 0}
+
+    <div class="flex flex-col gap-y-4 justify-center items-center mb-6 mx-2">
+        {#each writeRelays as relay(relay.url)}
+            <div class="card card-hover grid grid-cols-[1fr_auto] gap-x-2 bg-surface-active-token p-4">
+                <RelayListElement {relay}/>
+                <button 
+                    class="btn bg-error-500"
+                    type="button" 
+                    on:click={()=>{removeRelay(relay.url, false)}}>
+                    Remove
+                </button>
+            </div> 
+        {/each}
+        <label class="label flex flex-col items-center w-96 mb-6">
             <h3 class="h3">Add WRITE Relay</h3>
-            <input class="input w-full text-center" 
-            bind:value={writeRelayInputValue}
-            title="relay input (url)" 
-            type="text" 
-            placeholder="Enter Relay URL(without wss://)"
-        />
+            <div class="flex gap-x-2">
+                <input class="input w-full text-center" 
+                bind:value={writeRelayInputValue}
+                title="relay input (url)" 
+                type="text" 
+                placeholder="Relay URL, 'without wss://'"
+            />
+                <button 
+                    class="btn btn-icon"
+                    type="button" 
+                    on:click={()=>{addRelay(false)}}
+                    disabled={posting}
+                >
+                    {#if posting}
+                        <ProgressRadial
+                        value={undefined}
+                        stroke={60}
+                        meter="stroke-primary-500"
+                        track="stroke-primary-500/30"
+                        strokeLinecap="round" width="w-8" 
+                    />
+                    {:else} 
+                        <span>
+                            <i 
+                                class="text-3xl text-primary-500 fa-solid fa-circle-plus"
+                            >
+                            </i>
+                        </span>
+                    {/if}
+                </button>
+            </div>
         </label>
     </div>
-</div>
+{:else}
+    <div class="p-4 space-y-4">
+        <div class="grid grid-cols-3 gap-8">
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+        </div>
+        <div class="grid grid-cols-4 gap-4">
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+        </div>
+        <div class="grid grid-cols-3 gap-8">
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+        </div>
+    </div>
+{/if}
 <div class="flex justify-center mb-6" >
-    <button class="btn btn-md bg-primary-300-600-token" on:click={() => location.reload()}>
+    <button 
+        class="btn btn-md bg-primary-300-600-token" 
+        on:click={() => location.reload()}>
         Retry Connections
     </button>
 </div>
