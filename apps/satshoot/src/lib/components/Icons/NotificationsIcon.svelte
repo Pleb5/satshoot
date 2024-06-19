5ce647fc55749a769608e85625b675b0f81fff8c
Five <co22rkn@protonmail.com>
2024-06-19 09:09:25 +0000

    Notifications: Badge icon on top of bell app bar menu icon

--- apps/satshoot/src/lib/components/Icons/NotificationsIcon.svelte
+++ apps/satshoot/src/lib/components/Icons/NotificationsIcon.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
+import { notifications } from '$lib/stores/notifications';
+
     export let sizeClass = 'text-lg';
+
 </script>
 
-<span class="">
-    <i class="fa-solid fa-bell {sizeClass}"></i>
-</span>
+<div class="relative inline-block">
+    <span class="">
+        <i class="fa-solid fa-bell {sizeClass}"></i>
+    </span>
+
+    {#if $notifications?.length > 0}
+        <span 
+            class="badge-icon variant-filled-error absolute -top-1 -right-2 z-10"
+            style="font-size:8pt; width: 15px; height: 15px;"
+        >
+            {$notifications.length}
+        </span>
+    {/if}
+</div>
 
