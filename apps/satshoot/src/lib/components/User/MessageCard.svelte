854dfbd2844cccde687a13f8e09adf6c372592c9
Five <co22rkn@protonmail.com>
2024-07-15 09:11:35 +0200

    Message card: placeholder nicer

--- apps/satshoot/src/lib/components/User/MessageCard.svelte
+++ apps/satshoot/src/lib/components/User/MessageCard.svelte
@@ -133,21 +133,14 @@ $: if (decryptedDM) {
         {/if}
     </div>
 {:else} 
-    <div class="p-4 space-y-4">
-        <div class="grid grid-cols-3 gap-8">
-            <div class="placeholder animate-pulse" />
-            <div class="placeholder animate-pulse" />
+    <div class="p-4 space-y-4 w-32">
+        <div class="grid grid-cols-1">
             <div class="placeholder animate-pulse" />
         </div>
-        <div class="grid grid-cols-4 gap-4">
-            <div class="placeholder animate-pulse" />
-            <div class="placeholder animate-pulse" />
-            <div class="placeholder animate-pulse" />
+        <div class="grid grid-cols-1">
             <div class="placeholder animate-pulse" />
         </div>
-        <div class="grid grid-cols-3 gap-8">
-            <div class="placeholder animate-pulse" />
-            <div class="placeholder animate-pulse" />
+        <div class="grid grid-cols-1">
             <div class="placeholder animate-pulse" />
         </div>
     </div>
