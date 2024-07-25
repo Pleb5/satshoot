6612a0a63dad1bcc73fc66e3a0a63f202f33b2aa
Five <co22rkn@protonmail.com>
2024-07-25 07:47:37 +0000

    User page: Show deals of the current user with the displayed user, page sections into accordions

---
+++ apps/satshoot/src/lib/components/Icons/HandshakeIcon.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let sizeClass = 'text-lg';
+</script>
+
+<span class="">
+    <i class="fa-solid fa-handshake-simple {sizeClass}"></i>
+</span>
--- apps/satshoot/src/routes/[npub=user]/+page.svelte
+++ apps/satshoot/src/routes/[npub=user]/+page.svelte
@@ -3,14 +3,20 @@ import { page } from '$app/stores';
 import Reputation from '$lib/components/OrderBook/Reputation.svelte';
 import UserCard from "$lib/components/User/UserCard.svelte";
 import { TicketEvent } from '$lib/events/TicketEvent';
-import currentUser from '$lib/stores/user';
+import currentUser, { loggedIn } from '$lib/stores/user';
 import ndk from '$lib/stores/ndk';
 import { NDKKind } from '@nostr-dev-kit/ndk';
 import type { NDKEventStore, ExtendedBaseType } from '@nostr-dev-kit/ndk-svelte';
 import { OfferEvent } from '$lib/events/OfferEvent';
 import TicketCard from '$lib/components/OrderBook/TicketCard.svelte';
 import OfferCard from '$lib/components/OrderBook/OfferCard.svelte';
-    import { onDestroy, onMount } from 'svelte';
+import { onDestroy, onMount } from 'svelte';
+import { derived } from 'svelte/store';
+import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+import UserTicketsIcon from '$lib/components/Icons/UserTicketsIcon.svelte';
+import HandshakeIcon from '$lib/components/Icons/HandshakeIcon.svelte';
+import PostTicketIcon from '$lib/components/Icons/PostTicketIcon.svelte';
+import UserOffersIcon from '$lib/components/Icons/UserOffersIcon.svelte';
 
 $: npub = $page.params.npub;
 $: user = $ndk.getUser({npub: npub});
@@ -21,6 +27,9 @@ const subOptions = {
 
 let allTicketsOfUser:NDKEventStore<ExtendedBaseType<TicketEvent>>;
 let allOffersOfUser:NDKEventStore<ExtendedBaseType<OfferEvent>>;
+let ticketsWithUser;
+let offersWithUser;
+
 
 $: if (user) {
     if (allTicketsOfUser) allTicketsOfUser.empty();
@@ -45,6 +54,44 @@ $: if (user) {
     );
 }
 
+$: if (loggedIn) {
+    ticketsWithUser = derived(
+        [allTicketsOfUser, currentUser],
+        ([$allTicketsOfUser, $currentUser]) => {
+            const tickets = $allTicketsOfUser.filter((ticket: TicketEvent) => {
+                const winner = ticket.acceptedOfferAddress;
+                if (
+                    winner
+                    && winner.split(':')[1] === $currentUser!.pubkey
+                ) {
+                    return true;
+                } 
+
+                return false;
+            });
+
+            return tickets;
+        }
+    );
+
+    offersWithUser = derived(
+        [allOffersOfUser, currentUser],
+        ([$allOffersOfUser, $currentUser]) => {
+            const offers = $allOffersOfUser.filter((offer: OfferEvent) => {
+                const ticketPubkey = offer.referencedTicketAddress.split(':')[1];
+                if (ticketPubkey === $currentUser!.pubkey) {
+                    return true;
+                }
+
+                return false;
+            });
+
+            return offers;
+        }
+    );
+
+}
+
 onMount(() => {
     // Scroll to top as soon as ticket arrives
     const elemPage:HTMLElement = document.querySelector('#page') as HTMLElement;
@@ -55,7 +102,6 @@ onDestroy(()=>{
     if (allTicketsOfUser) allTicketsOfUser.empty();
     if (allOffersOfUser) allOffersOfUser.empty();
 });
-     
 
 </script>
 
@@ -71,40 +117,148 @@ onDestroy(()=>{
     </div>
     {/if}
 
-    <h3 class="h3 text-center underline">
-        Tickets of User
-    </h3>
-    {#if $allTicketsOfUser.length > 0}
-        <div class="flex flex-col items-center gap-y-8">
-            {#each $allTicketsOfUser as ticket (ticket.id)}
-                <div class="w-[90vw] sm:w-[70vw] lg:w-[60vw]">
-                    <TicketCard
-                    {ticket}
-                    titleSize={'md lg:text-xl'}
-                    showReputation={false}
-                />
-                </div>
-            {/each}
-        </div>
-    {:else}
-        <h4>No Tickets found!</h4>
-    {/if}
-    <h3 class="h3 text-center underline">
-        Offers of User
-    </h3>
-    {#if $allOffersOfUser.length > 0}
-        <div class="flex flex-col items-center gap-y-8">
-            {#each $allOffersOfUser as offer (offer.id)}
-                <div class="w-[90vw] sm:w-[70vw] lg:w-[60vw]">
-                    <OfferCard 
-                    {offer}
-                    enableChat={false}
-                    showReputation={false}
-                />
-                </div>
-            {/each}
-        </div>
-    {:else}
-        <h4>No Offers found!</h4>
-    {/if}
+    <div class="w-[90vw] sm:w-[70vw] lg:w-[60vw]">
+        {#if $loggedIn && (user.pubkey !== $currentUser?.pubkey)}
+            {#if $ticketsWithUser?.length > 0 
+                || $offersWithUser?.length > 0}
+                <Accordion>
+                    <AccordionItem>
+                        <svelte:fragment slot="lead">
+                            <HandshakeIcon/>
+                        </svelte:fragment>
+                        <svelte:fragment slot="summary">
+                            <h3 class="h3 text-center underline">
+                                Deals with User
+                            </h3>
+                        </svelte:fragment>
+                        <svelte:fragment slot="content">
+                            <Accordion>
+                                <AccordionItem>
+                                    <svelte:fragment slot="lead">
+                                        <PostTicketIcon/>
+                                    </svelte:fragment>
+                                    <svelte:fragment slot="summary">
+                                        <h3 class="h3 text-center underline">
+                                            Tickets
+                                        </h3>
+                                    </svelte:fragment>
+                                    <svelte:fragment slot="content">
+                                        {#if $ticketsWithUser?.length > 0}
+                                            <div class="flex flex-col items-center gap-y-8">
+                                                {#each $ticketsWithUser as ticket (ticket.id)}
+                                                    <div class="w-[90vw] sm:w-[70vw] lg:w-[60vw]">
+                                                        <TicketCard
+                                                        {ticket}
+                                                        titleSize={'md lg:text-xl'}
+                                                        showReputation={false}
+                                                    />
+                                                    </div>
+                                                {/each}
+                                            </div>
+                                        {:else}
+                                            <h4 class="text-center">No Tickets with User!</h4>
+                                        {/if}
+                                    </svelte:fragment>
+                                </AccordionItem>
+                            </Accordion>
+                            <Accordion>
+                                <AccordionItem>
+                                    <svelte:fragment slot="lead">
+                                        <UserOffersIcon/>
+                                    </svelte:fragment>
+                                    <svelte:fragment slot="summary">
+                                        <h3 class="h3 text-center underline">
+                                            Offers
+                                        </h3>
+                                    </svelte:fragment>
+                                    <svelte:fragment slot="content">
+                                        {#if $offersWithUser?.length > 0}
+                                            <div class="flex flex-col items-center gap-y-8">
+                                                {#each $offersWithUser as offer (offer.id)}
+                                                    <div class="w-[90vw] sm:w-[70vw] lg:w-[60vw]">
+                                                        <OfferCard 
+                                                        {offer}
+                                                        enableChat={false}
+                                                        showReputation={false}
+                                                    />
+                                                    </div>
+                                                {/each}
+                                            </div>
+                                        {:else}
+                                            <h4 class="text-center">No Offers found!</h4>
+                                        {/if}
+                                    </svelte:fragment>
+                                </AccordionItem>
+                            </Accordion>
+                        </svelte:fragment>
+                    </AccordionItem>
+                </Accordion>
+            {:else}
+                <h3 class="h3 text-center">
+                    No Deals yet with this User
+                </h3>
+            {/if}
+            
+        {:else if (user.pubkey !== $currentUser?.pubkey)}
+            <div class="h4">Login to view your deals with this user!</div>
+        {/if}
+        <Accordion>
+            <AccordionItem>
+                <svelte:fragment slot="lead">
+                    <UserTicketsIcon/>
+                </svelte:fragment>
+                <svelte:fragment slot="summary">
+                    <h3 class="h3 text-center underline">
+                        All Tickets of User
+                    </h3>
+                </svelte:fragment>
+                <svelte:fragment slot="content">
+                    {#if $allTicketsOfUser.length > 0}
+                        <div class="flex flex-col items-center gap-y-8">
+                            {#each $allTicketsOfUser as ticket (ticket.id)}
+                                <div class="w-[90vw] sm:w-[70vw] lg:w-[60vw]">
+                                    <TicketCard
+                                    {ticket}
+                                    titleSize={'md lg:text-xl'}
+                                    showReputation={false}
+                                />
+                                </div>
+                            {/each}
+                        </div>
+                    {:else}
+                        <h4 class="text-center">No Tickets found!</h4>
+                    {/if}
+                </svelte:fragment>
+            </AccordionItem>
+        </Accordion>
+        <Accordion>
+            <AccordionItem>
+                <svelte:fragment slot="lead">
+                    <UserOffersIcon/>
+                </svelte:fragment>
+                <svelte:fragment slot="summary">
+                    <h3 class="h3 text-center underline">
+                        All Offers of User
+                    </h3>
+                </svelte:fragment>
+                <svelte:fragment slot="content">
+                    {#if $allOffersOfUser.length > 0}
+                        <div class="flex flex-col items-center gap-y-8">
+                            {#each $allOffersOfUser as offer (offer.id)}
+                                <div class="w-[90vw] sm:w-[70vw] lg:w-[60vw]">
+                                    <OfferCard 
+                                    {offer}
+                                    enableChat={false}
+                                    showReputation={false}
+                                />
+                                </div>
+                            {/each}
+                        </div>
+                    {:else}
+                        <h4 class="text-center">No Offers found!</h4>
+                    {/if}
+                </svelte:fragment>
+            </AccordionItem>
+        </Accordion>
+    </div>
 </div>
