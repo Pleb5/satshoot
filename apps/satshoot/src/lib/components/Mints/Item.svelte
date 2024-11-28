2242d460f59d06d9f1146de0c099d5caf7dc401b
daniyal <daniyal@nostrdev.com>
2024-11-28 17:13:15 +0500

    fix: Only allow user to add Cashu Mints that handle 'sat' unit and only pay mints that accept 'sat' unit

--- apps/satshoot/src/lib/components/Mints/Item.svelte
+++ apps/satshoot/src/lib/components/Mints/Item.svelte
@@ -11,15 +11,21 @@
     export let isSelected = false;
 
     let mintInfo: GetInfoResponse | null = null;
+    let gettingMintInfo = true;
 
     $: {
         const mint = new CashuMint(mintUrl);
         mint.getInfo()
             .then((info) => {
-                mintInfo = info;
+                if (info.nuts[4].methods.some((method) => method.unit === 'sat')) {
+                    mintInfo = info;
+                }
             })
             .catch((err) => {
                 console.error('An error occurred in getting mint info', mintUrl, err);
+            })
+            .finally(() => {
+                gettingMintInfo = false;
             });
     }
 
@@ -39,52 +45,63 @@
     }
 </script>
 
-<div
-    class="flex justify-between p-2 pr-4 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer"
-    role="button"
-    on:click={toggleSelection}
-    on:keydown={handleKeydown}
-    tabindex="0"
->
-    <div class="flex flex-col gap-2 w-full">
-        <div class="flex justify-between">
-            <h3 class="text-foreground font-bold">
-                {#if mintInfo}
+{#if gettingMintInfo}
+    <div class="p-4 space-y-4 w-full">
+        <div class="placeholder animate-pulse" />
+        <div class="grid grid-cols-3 gap-8">
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+        </div>
+        <div class="grid grid-cols-4 gap-4">
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+            <div class="placeholder animate-pulse" />
+        </div>
+    </div>
+{:else if mintInfo}
+    <div
+        class="flex justify-between p-2 pr-4 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer"
+        role="button"
+        on:click={toggleSelection}
+        on:keydown={handleKeydown}
+        tabindex="0"
+    >
+        <div class="flex flex-col gap-2 w-full">
+            <div class="flex justify-between">
+                <h3 class="text-foreground font-bold">
                     <span>
                         {mintInfo.name.length < 26
                             ? mintInfo.name
                             : mintInfo.name.substring(0, 25) + '...'}
                     </span>
-                {:else}
-                    <span>
-                        {mintUrl.length < 26 ? mintUrl : mintUrl.substring(0, 25) + '...'}
-                    </span>
-                {/if}
-            </h3>
-            <input type="checkbox" class="checkbox" bind:checked={isSelected} />
-        </div>
-        <div class="text-muted-foreground">
-            {mintInfo?.description || ''}
-        </div>
-
-        {#if mintUsage.pubkeys.size > 0}
-            <div class="flex flex-row items-center gap-2">
-                <div class="flex flex-row -space-x-2">
-                    {#each Array.from(mintUsage.pubkeys).slice(0, 4) as pubkey}
-                        <Avatar {pubkey} size="tiny" />
-                    {/each}
-                </div>
-                <div class="flex justify-between items-center gap-x-1">
-                    {#if mintUsage.pubkeys.size > 4}
-                        <span
-                            class="bg-secondary text-foreground flex h-6 w-6 items-center justify-center rounded-full text-xs"
-                        >
-                            +{mintUsage.pubkeys.size - 4}
-                        </span>
-                    {/if}
-                    <span class="text-muted-foreground text-xs"> recommendations </span>
-                </div>
+                </h3>
+                <input type="checkbox" class="checkbox" bind:checked={isSelected} />
             </div>
-        {/if}
+            <div class="text-muted-foreground">
+                {mintInfo.description || ''}
+            </div>
+
+            {#if mintUsage.pubkeys.size > 0}
+                <div class="flex flex-row items-center gap-2">
+                    <div class="flex flex-row -space-x-2">
+                        {#each Array.from(mintUsage.pubkeys).slice(0, 4) as pubkey}
+                            <Avatar {pubkey} size="tiny" />
+                        {/each}
+                    </div>
+                    <div class="flex justify-between items-center gap-x-1">
+                        {#if mintUsage.pubkeys.size > 4}
+                            <span
+                                class="bg-secondary text-foreground flex h-6 w-6 items-center justify-center rounded-full text-xs"
+                            >
+                                +{mintUsage.pubkeys.size - 4}
+                            </span>
+                        {/if}
+                        <span class="text-muted-foreground text-xs"> recommendations </span>
+                    </div>
+                </div>
+            {/if}
+        </div>
     </div>
-</div>
+{/if}
