3c25081f7439f436ac656c183be448afa11468b1
Five <co22rkn@protonmail.com>
2025-03-04 13:13:09 +0100

    Footer UI adjustments: only show one type of app menu, better icon positioning and experiment Chrome mobile still barely showing the footer

--- apps/satshoot/src/lib/components/layout/Footer.svelte
+++ apps/satshoot/src/lib/components/layout/Footer.svelte
@@ -5,6 +5,8 @@
     import SearchModal from '../Modals/SearchModal.svelte';
     import BottomNavItem from './BottomNavItem.svelte';
 
+    export let hideFooter = false;
+
     const modalStore = getModalStore();
 
     $: searchQuery = $page.url.searchParams.get('searchTerms');
@@ -42,48 +44,23 @@
         'bg-gray-600 dark:bg-brightGray border-[2px] border-white dark:border-white-100 ' +
         'flex flex-row items-center gap-[10px] p-[5px] rounded-[10px] shadow-soft';
 
-    const itemClasses =
-        'transition ease-in-out duration-[0.3s] ' +
-        'text-[22px] border-0 outline-none py-[10px] px-[15px] ' +
-        'rounded-[5px] font-semibold transform scale-100 whitespace-nowrap flex flex-row ' +
-        'justify-center items-center gap-[8px] hover:bg-blue-600 hover:text-white relative';
 </script>
 
-<div class="flex justify-center relative">
+<div class="flex flex-col items-center min-h-[70px] h-full">
     {#if filterList.length > 0}
         <div
-            class="p-[4px] bg-white dark:bg-brightGray rounded-[4px] shadow-subtle flex flex-row absolute top-[-25px]"
+            class="p-[4px] bg-white dark:bg-brightGray rounded-[4px] shadow-subtle flex flex-row"
         >
             <p class="w-full text-[14px] font-[500] text-center text-black-500 dark:text-white-500">
                 There's currently an active search on this page
             </p>
         </div>
     {/if}
-
-    <div
-        class="hidden w-full min-h-[7dvh] max-[576px]:flex flex-row justify-between items-center gap-[5px] p-[5px] border-t border-gray-300 bg-white dark:bg-brightGray dark:border-t-white-100 relative"
-    >
-        <div class="flex justify-center flex-grow">
-            {#each items as { href, icon }}
-                {#if icon === 'bx-search'}
-                    <BottomNavItem
-                        {href}
-                        {icon}
-                        isActive={filterList.length !== 0}
-                        on:click={handleSearch}
-                    />
-                {:else}
-                    <BottomNavItem {href} {icon} isActive={href === $page.url.pathname} />
-                {/if}
-            {/each}
-        </div>
-    </div>
-
-    <div
-        class="max-[576px]:hidden min-h-[7dvh]  flex flex-grow justify-center py-[5px] bg-black-50 dark:bg-brightGray-700 relative"
-    >
-        <div class="flex justify-center">
-            <div class={wrapperClasses}>
+    <div class="{hideFooter ? 'hidden' : ''} flex justify-center w-full h-full">
+        <div
+            class="w-full flex flex-row justify-between items-center gap-[5px] p-[5px] border-t border-gray-300 bg-white dark:bg-brightGray dark:border-t-white-100 relative"
+        >
+            <div class="flex justify-center flex-grow">
                 {#each items as { href, icon }}
                     {#if icon === 'bx-search'}
                         <BottomNavItem
@@ -96,6 +73,27 @@
                         <BottomNavItem {href} {icon} isActive={href === $page.url.pathname} />
                     {/if}
                 {/each}
+            </div>
+        </div>
+
+        <div
+            class="hidden min-h-[55px] flex flex-grow justify-center py-[5px] bg-black-50 dark:bg-brightGray-700 relative"
+        >
+            <div class="flex justify-center">
+                <div class={wrapperClasses}>
+                    {#each items as { href, icon }}
+                        {#if icon === 'bx-search'}
+                            <BottomNavItem
+                                {href}
+                                {icon}
+                                isActive={filterList.length !== 0}
+                                on:click={handleSearch}
+                            />
+                        {:else}
+                            <BottomNavItem {href} {icon} isActive={href === $page.url.pathname} />
+                        {/if}
+                    {/each}
+                </div>
             </div>
         </div>
     </div>
