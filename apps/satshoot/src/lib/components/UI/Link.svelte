0ade27314f4cf102ba5575b3ea1bc40e2614390d
Five <co22rkn@protonmail.com>
2025-03-09 10:43:33 +0100

    Improved Rendering in Markdown.svelte: - Render nostr and other entities inside list items - Render naddr-s with in-app link only when kind matches job kind - Render external links with target: "_blank" - Add rel="noopener noreferrer" in anchor tag for security - Rename badly named 'npub' to 'pubkey' in getPub() The only case that is NOT covered is when the user clicks a job link in a different 	job's page. The page will not reload to the other job in this scenario but 	this should be a rare edge case. The solution would involve a more deeply split approach for 	internal and external entities and a custom Link component handling external/internal logic - The basis for handling Links better is contained in Link.svelte but it is NOT used for now

---
+++ apps/satshoot/src/lib/components/UI/Link.svelte
@@ -0,0 +1,23 @@
+<script lang='ts'>
+    import { goto } from '$app/navigation';
+    
+    export let url = '';
+    export let external = false;
+    
+    const handleClick = (e: Event) => {
+        if (!external) {
+            e.preventDefault();
+            goto(url, { replaceState: false});
+        }
+    };
+</script>
+
+<a
+    href={url}
+    class="text-blue-600 hover:text-blue-800 hover:underline"
+    on:click={handleClick}
+    rel={external ? 'noopener noreferrer' : ''}
+    target={external ? '_blank' : ''}
+>
+    <slot></slot>
+</a>
