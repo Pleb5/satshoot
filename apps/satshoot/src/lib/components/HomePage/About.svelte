1d32df027f1a6990e0dbcaf3194921e97afc66f3
Five <co22rkn@protonmail.com>
2025-03-10 04:29:03 +0100

    Home page about section title renamed

--- apps/satshoot/src/lib/components/HomePage/About.svelte
+++ apps/satshoot/src/lib/components/HomePage/About.svelte
@@ -6,7 +6,7 @@
     <div class="max-w-[1400px] w-full flex flex-col justify-start items-end px-[10px] relative">
         <div class="w-full flex flex-col gap-[50px] max-[576px]:gap-[25px]">
             <div class="w-full flex flex-col gap-[15px]">
-                <h2 class="text-[40px] font-[500] text-center">About</h2>
+                <h2 class="text-[40px] font-[500] text-center">Why SatShoot?</h2>
             </div>
             <Card classes="justify-center items-center text-center text-[18px] p-[25px]">
                 <p>
