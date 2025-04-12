c25fb56bd7e6f9686ae29c45e3969ad48c04cc60
daniyal <daniyal@nostrdev.com>
2025-04-12 22:37:21 +0500

    chore: fix darktheme toggle and adjusted the brightGray color name in global .css file

--- apps/satshoot/src/lib/components/UI/Inputs/LightSwitch.svelte
+++ apps/satshoot/src/lib/components/UI/Inputs/LightSwitch.svelte
@@ -17,13 +17,6 @@
     };
 </script>
 
-<svelte:head>
-    <script>
-        const mode = localStorage.getItem('mode') || 'light';
-        document.documentElement.setAttribute('data-mode', mode);
-    </script>
-</svelte:head>
-
 <Switch {checked} {onCheckedChange}>
     {#snippet inactiveChild()}<i class="bx bxs-sun"></i>{/snippet}
     {#snippet activeChild()}<i class="bx bxs-moon"></i>{/snippet}
