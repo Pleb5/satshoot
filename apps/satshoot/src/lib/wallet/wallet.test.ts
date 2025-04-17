655e36ea78fe00c54882edc91172d7cca920a449
daniyal <daniyal@nostrdev.com>
2025-04-17 23:59:56 +0500

    chore: fix wallet.test.ts

--- apps/satshoot/src/lib/wallet/wallet.test.ts
+++ apps/satshoot/src/lib/wallet/wallet.test.ts
@@ -1,14 +1,12 @@
 import type { Proof } from '@cashu/cashu-ts';
-import { NDKPrivateKeySigner } from '@nostr-dev-kit/ndk';
+import { NDKCashuMintList, NDKPrivateKeySigner, NDKRelaySet } from '@nostr-dev-kit/ndk';
 import NDKSvelte from '@nostr-dev-kit/ndk-svelte';
 import { NDKCashuWallet, NDKWalletStatus } from '@nostr-dev-kit/ndk-wallet';
 import { get } from 'svelte/store';
 
-import {
-    wallet,
-    walletInit,
-    walletStatus 
-} from './wallet';
+import { wallet, walletInit, walletStatus } from './wallet';
+import { serializeWallet, WALLET_BACKUP_VERSION, type WalletStorage } from './cashu';
+import { DEFAULTRELAYURLS } from '$lib/stores/ndk';
 
 describe('walletStatus', () => {
     test('should be `INITIAL` by default', () => {
@@ -26,11 +24,11 @@ describe('walletBackup', () => {
         localStorage.clear();
     });
 
-    test('should be of size 0 by default', async () => {
+    test('should be null by default', async () => {
         // Dynamically import the store after clearing localStorage
         const { walletBackup } = await import('./wallet');
 
-        expect(get(walletBackup).size).toBe(0);
+        expect(get(walletBackup)).toBeNull();
     });
 
     test('should also update local storage on updating store', async () => {
@@ -51,25 +49,28 @@ describe('walletBackup', () => {
                 C: 'mock value',
             },
         ];
+
         const mockMint1 = 'wss://mock-mint-1.com';
         const mockMint2 = 'wss://mock-mint-2.com';
-        const stringified = JSON.stringify([
-            [mockMint1, mockProofs],
-            [mockMint2, mockProofs],
-        ]);
 
-        walletBackup.update((map) => {
-            map.set(mockMint1, mockProofs);
-            map.set(mockMint2, mockProofs);
-            return map;
-        });
+        const ws: WalletStorage = {
+            version: WALLET_BACKUP_VERSION,
+            cashuKeys: {},
+            mintsWithProofs: {
+                [mockMint1]: mockProofs,
+                [mockMint2]: mockProofs,
+            },
+        };
+
+        walletBackup.set(ws);
+        const serializedWallet = serializeWallet(ws);
 
         const $walletBackup = get(walletBackup);
 
-        expect($walletBackup.size).toEqual(2);
-        expect($walletBackup.get(mockMint1)).toEqual(mockProofs);
-        expect($walletBackup.get(mockMint2)).toEqual(mockProofs);
-        expect(localStorage.getItem('walletBackup')).toEqual(stringified);
+        expect(Object.entries($walletBackup?.mintsWithProofs || {}).length).toEqual(2);
+        expect($walletBackup?.mintsWithProofs[mockMint1]).toEqual(mockProofs);
+        expect($walletBackup?.mintsWithProofs[mockMint2]).toEqual(mockProofs);
+        expect(localStorage.getItem('walletBackup')).toEqual(serializedWallet);
     });
 
     test('should load with existing value in local storage', async () => {
@@ -89,68 +90,52 @@ describe('walletBackup', () => {
         ];
         const mockMint1 = 'wss://mock-mint-1.com';
         const mockMint2 = 'wss://mock-mint-2.com';
-        const stringified = JSON.stringify([
-            [mockMint1, mockProofs],
-            [mockMint2, mockProofs],
-        ]);
-        localStorage.setItem('walletBackup', stringified);
+
+        const ws: WalletStorage = {
+            version: WALLET_BACKUP_VERSION,
+            cashuKeys: {},
+            mintsWithProofs: {
+                [mockMint1]: mockProofs,
+                [mockMint2]: mockProofs,
+            },
+        };
+
+        const serializedWallet = serializeWallet(ws);
+
+        localStorage.setItem('walletBackup', serializedWallet);
 
         // Dynamically import the store after setting localStorage
         const { walletBackup } = await import('./wallet');
 
         const $walletBackup = get(walletBackup);
 
-        expect(localStorage.getItem('walletBackup')).toEqual(stringified);
-        expect($walletBackup.size).toEqual(2);
-        expect($walletBackup.get(mockMint1)).toEqual(mockProofs);
-        expect($walletBackup.get(mockMint2)).toEqual(mockProofs);
-    });
-});
-
-describe('ndkWalletService', () => {
-    test('should be an instance of NDKWalletService', async () => {
-        const ndk = new NDKSvelte();
-        const user = await NDKPrivateKeySigner.generate().user();
-        walletInit(ndk, user);
-
-        const { ndkWalletService } = await import('./wallet');
-
-        expect(get(ndkWalletService)).toBeInstanceOf(NDKWalletService);
+        expect(localStorage.getItem('walletBackup')).toEqual(serializedWallet);
+        expect(Object.entries($walletBackup?.mintsWithProofs || {}).length).toEqual(2);
+        expect($walletBackup?.mintsWithProofs[mockMint1]).toEqual(mockProofs);
+        expect($walletBackup?.mintsWithProofs[mockMint2]).toEqual(mockProofs);
     });
 });
 
 describe('walletInit', () => {
-    afterEach(() => {
-        vi.restoreAllMocks();
-    });
-
     it('should initialize the wallet and update the store', async () => {
-        const mockSubscribeForNutZaps = vi.fn(); // Mock implementation
-
-        // Mock `start` method
-        vi.spyOn(NDKWalletService.prototype, 'start').mockImplementation(() => {});
-
         const ndk = new NDKSvelte();
         const user = await NDKPrivateKeySigner.generate().user();
 
-        // Call walletInit with mocked `subscribeForNutZaps`
-        walletInit(ndk, user, mockSubscribeForNutZaps);
+        const newWallet = new NDKCashuWallet(ndk);
+        await newWallet.getP2pk();
+        newWallet.relaySet = NDKRelaySet.fromRelayUrls(DEFAULTRELAYURLS, ndk);
+        // todo: set mints
 
-        // Assertions: Validate `ndkWalletService` initialization
-        const $ndkWalletService = get(ndkWalletService);
-        expect($ndkWalletService).toBeInstanceOf(NDKWalletService);
+        const cashuInfo = new NDKCashuMintList(ndk);
+        cashuInfo.p2pk = newWallet._p2pk;
+        cashuInfo.relays = DEFAULTRELAYURLS;
+        cashuInfo.mints = newWallet.mints;
 
-        // Emit `wallet:default` to simulate the wallet event
-        const mockWallet = new NDKCashuWallet(ndk);
-        $ndkWalletService?.emit('wallet:default', mockWallet);
+        walletInit(newWallet, cashuInfo, ndk, user);
 
         // Validate the `wallet` store
         const $wallet = get(wallet);
         expect($wallet).toBeDefined();
         expect($wallet).toBeInstanceOf(NDKCashuWallet);
-
-        // Ensure `subscribeForNutZaps` was called
-        expect(mockSubscribeForNutZaps).toHaveBeenCalledTimes(1);
-        expect(mockSubscribeForNutZaps).toHaveBeenCalledWith(ndk, user, mockWallet);
     });
 });
--- apps/satshoot/src/lib/wallet/wallet.ts
+++ apps/satshoot/src/lib/wallet/wallet.ts
@@ -1,14 +1,7 @@
-import { getCashuPaymentInfo } from '$lib/utils/helpers';
 import { NDKCashuMintList, NDKUser, type CashuPaymentInfo } from '@nostr-dev-kit/ndk';
 import type NDKSvelte from '@nostr-dev-kit/ndk-svelte';
-import {
-    NDKCashuWallet,
-    NDKNutzapMonitor,
-    NDKWalletStatus,
-} from '@nostr-dev-kit/ndk-wallet';
-import { derived, writable, type Readable } from 'svelte/store';
-import { myTickets } from '$lib/stores/freelance-eventstores';
-import { SatShootPubkey } from '$lib/utils/misc';
+import { NDKCashuWallet, NDKNutzapMonitor, NDKWalletStatus } from '@nostr-dev-kit/ndk-wallet';
+import { writable } from 'svelte/store';
 import { persisted } from 'svelte-persisted-store';
 import { get } from 'svelte/store';
 import {
@@ -16,12 +9,12 @@ import {
     recoverWallet,
     serializeWallet,
     toWalletStorage,
-    type WalletStorage 
+    type WalletStorage,
 } from './cashu';
 
 export const wallet = writable<NDKCashuWallet | null>(null);
 export let userCashuInfo = writable<NDKCashuMintList | null>(null);
-export let ndkNutzapMonitor:NDKNutzapMonitor | null = null;
+export let ndkNutzapMonitor: NDKNutzapMonitor | null = null;
 
 export const walletStatus = writable<NDKWalletStatus>(NDKWalletStatus.INITIAL);
 
@@ -30,50 +23,46 @@ export const walletStatus = writable<NDKWalletStatus>(NDKWalletStatus.INITIAL);
 // The backup writes to local storage, and state consolidation(internal and external)
 // is carried out on each wallet initialization (runs after import, session restart etc)
 // Does NOT need any async calls before saving state (No signing of an event etc)
-export const walletBackup = persisted<WalletStorage | null>(
-    'walletBackup',
-    null,
-    {
-        storage: 'local',
-        serializer: {
-            stringify(walletStorage: WalletStorage) {
-                return serializeWallet(walletStorage)
-            },
-            parse(text) {
-                return parseAndValidateBackup(text)
-            },
+export const walletBackup = persisted<WalletStorage | null>('walletBackup', null, {
+    storage: 'local',
+    serializer: {
+        stringify(walletStorage: WalletStorage) {
+            return serializeWallet(walletStorage);
         },
-    }
-);
+        parse(text) {
+            return parseAndValidateBackup(text);
+        },
+    },
+});
 
 export async function walletInit(
     nostrWallet: NDKCashuWallet,
-    mintList:NDKCashuMintList,
+    mintList: NDKCashuMintList,
     ndk: NDKSvelte,
-    user: NDKUser,
+    user: NDKUser
 ) {
     if (!nostrWallet._p2pk) {
         await nostrWallet.getP2pk();
     }
 
     wallet.set(nostrWallet);
-    userCashuInfo.set(mintList)
+    userCashuInfo.set(mintList);
 
-    nostrWallet.on("ready", async () => {
+    nostrWallet.on('ready', async () => {
         walletStatus.set(NDKWalletStatus.READY);
 
-        ndkNutzapMonitor = new NDKNutzapMonitor(ndk, user, {mintList});
+        ndkNutzapMonitor = new NDKNutzapMonitor(ndk, user, { mintList });
         ndkNutzapMonitor.wallet = nostrWallet;
 
         ndkNutzapMonitor.on('seen', (nutzapEvent) => {
             console.log('nutzapEvent :>> ', nutzapEvent);
         });
 
-        // If we already have a backup in local storage, try to 
+        // If we already have a backup in local storage, try to
         // load Proofs, sync up and add p2pk privkeys to nutzapMonitor
         const $walletBackup = get(walletBackup);
         if ($walletBackup) {
-            await recoverWallet($walletBackup, nostrWallet, ndkNutzapMonitor)
+            await recoverWallet($walletBackup, nostrWallet, ndkNutzapMonitor);
         }
 
         ndkNutzapMonitor.start({
@@ -82,8 +71,8 @@ export async function walletInit(
         });
     });
 
-    nostrWallet.on("balance_updated", () => {
-        const $wallet = get(wallet); 
+    nostrWallet.on('balance_updated', () => {
+        const $wallet = get(wallet);
         if ($wallet) {
             console.info('Balance updated, saving backup...', $wallet.balance);
             // Save entire wallet state on the following operations:
@@ -94,6 +83,5 @@ export async function walletInit(
         }
     });
 
-    nostrWallet.start({subId: 'wallet', pubkey: user.pubkey});
+    nostrWallet.start({ subId: 'wallet', pubkey: user.pubkey });
 }
-
