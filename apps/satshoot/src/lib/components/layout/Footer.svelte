846f4f57d407bc6f6eb780317db38c3dcf57591f
daniyal <daniyal@nostrdev.com>
2025-04-30 21:32:55 +0500

    feat: move search functionality to top header and integrated fuse.js

--- apps/satshoot/src/lib/components/layout/Footer.svelte
+++ apps/satshoot/src/lib/components/layout/Footer.svelte
@@ -1,17 +1,6 @@
 <script lang="ts">
     import { page } from '$app/state';
-    import { Modal } from '@skeletonlabs/skeleton-svelte';
-    import SearchModal from '../Modals/SearchModal.svelte';
     import BottomNavItem from './BottomNavItem.svelte';
-
-    let isSearchModalOpened = $state(false);
-
-    let searchQuery = $derived(page.url.searchParams.get('searchTerms'));
-    let filterList = $derived(searchQuery ? searchQuery.split(',') : []);
-
-    function handleSearch() {
-        isSearchModalOpened = true;
-    }
 
     const items = [
         {
@@ -23,9 +12,6 @@
             icon: 'bxs-conversation',
         },
         {
-            icon: 'bx-search',
-        },
-        {
             href: '/notifications/',
             icon: 'bxs-bell',
         },
@@ -33,31 +19,13 @@
 </script>
 
 <div class="flex flex-col items-center max-sm:w-full">
-    {#if filterList.length > 0}
-        <div class="p-[4px] bg-white dark:bg-brightGray rounded-[4px] shadow-subtle flex flex-row">
-            <p class="w-full text-[14px] font-[500] text-center text-black-500 dark:text-white">
-                There's currently an active search on this page
-            </p>
-        </div>
-    {/if}
     <div
         class="w-full flex justify-center items-center gap-[5px] p-1 border-t border-gray-300 bg-white dark:bg-brightGray dark:border-t-white-100 relative rounded-tr-[10px] rounded-tl-[10px]"
     >
         <div class="flex justify-center gap-x-2">
             {#each items as { href, icon }}
-                {#if icon === 'bx-search'}
-                    <BottomNavItem
-                        {href}
-                        {icon}
-                        isActive={filterList.length !== 0}
-                        onClick={handleSearch}
-                    />
-                {:else}
-                    <BottomNavItem {href} {icon} isActive={href === page.url.pathname} />
-                {/if}
+                <BottomNavItem {href} {icon} isActive={href === page.url.pathname} />
             {/each}
         </div>
     </div>
 </div>
-
-<SearchModal bind:isOpen={isSearchModalOpened} />
--- apps/satshoot/src/lib/components/layout/Header.svelte
+++ apps/satshoot/src/lib/components/layout/Header.svelte
@@ -10,6 +10,10 @@
     import ndk, { BOOTSTRAPOUTBOXRELAYS, DEFAULTRELAYURLS } from '$lib/stores/session';
     import ProgressRing from '../UI/Display/ProgressRing.svelte';
     import AppMenu from './AppMenu.svelte';
+    import Input from '../UI/Inputs/input.svelte';
+    import { page } from '$app/state';
+    import { beforeNavigate, goto } from '$app/navigation';
+    import { onDestroy, onMount } from 'svelte';
 
     interface Props {
         onRestoreLogin: () => void;
@@ -23,6 +27,41 @@
     let profilePicture = $state('');
     let hasAttemptedProfileFetch = $state(false);
 
+    let searchInput = $state('');
+    let displaySearchInput = $state(false); // for mobile view
+
+    let debounceTimer = $state<NodeJS.Timeout | null>(null);
+
+    function handleSearch() {
+        // Clear any existing timer
+        if (debounceTimer) {
+            clearTimeout(debounceTimer);
+        }
+
+        // Set a new timer
+        debounceTimer = setTimeout(() => {
+            const url = new URL(page.url);
+
+            if (searchInput.trim()) {
+                url.searchParams.set('searchQuery', searchInput.trim());
+            } else {
+                url.searchParams.delete('searchQuery');
+            }
+
+            goto(url.toString(), { replaceState: true, keepFocus: true });
+        }, 300); // 300ms debounce delay
+    }
+
+    function clearSearch() {
+        searchInput = '';
+        // Get the current query parameters
+        const url = new URL(page.url);
+        // delete search query param
+        url.searchParams.delete('searchQuery');
+        // Navigate to the updated
+        goto(url.toString(), { replaceState: true, keepFocus: true });
+    }
+
     // Only trigger profile fetch when logged in changes to true and we haven't attempted yet
     $effect(() => {
         if ($loggedIn && !hasAttemptedProfileFetch) {
@@ -31,6 +70,29 @@
         } else if (!$loggedIn) {
             // Reset the flag when logged out, so we fetch again on next login
             hasAttemptedProfileFetch = false;
+        }
+    });
+
+    onMount(() => {
+        const searchQueryParam = page.url.searchParams.get('searchQuery');
+        if (searchQueryParam) searchInput = searchQueryParam;
+    });
+
+    // Clean up timer on component destroy
+    onDestroy(() => {
+        if (debounceTimer) {
+            clearTimeout(debounceTimer);
+        }
+    });
+
+    beforeNavigate(({ from, to }) => {
+        const trimmedSearchInput = searchInput.trim();
+        const searchQueryParam = to?.url.searchParams.get('searchQuery');
+
+        if (trimmedSearchInput) {
+            to?.url.searchParams.set('searchQuery', trimmedSearchInput);
+        } else if (searchQueryParam) {
+            searchInput = searchQueryParam;
         }
     });
 
@@ -79,41 +141,100 @@
                 class="max-w-[1400px] w-full flex flex-col justify-start items-end px-[10px] relative"
             >
                 <div class="w-full flex flex-row gap-6">
-                    <div class={satShootLogoWrapperClass}>
-                        <a href="/" class={satShootLogoClass}>
-                            <img
-                                src="/img/satshoot.svg"
-                                alt="Satshoot logo"
-                                class="w-full max-w-[45px]"
-                            />
-                            <p class="max-[576px]:hidden">SatShoot</p>
-                        </a>
-                    </div>
-                    <div class="flex flex-row grow gap-4 justify-end items-center">
-                        {#if $loggedIn}
-                            <Button href="/post-job/">Submit Job</Button>
-                            <button onclick={() => (showAppMenu = !showAppMenu)}>
-                                <!-- Avatar image -->
-                                <Avatar
-                                    classes="rounded-full border-white placeholder-white cursor-pointer"
-                                    size="size-14"
-                                    border="border-4 border-surface-300-600 hover:border-primary-500!"
-                                    src={profilePicture ??
-                                        getRoboHashPicture($currentUser?.pubkey ?? '')}
-                                    name={$currentUser?.profile?.displayName ?? ''}
+                    {#if !displaySearchInput}
+                        <!-- In mobile view when displaySearchInput is false, simply display normal header-->
+                        <div class={satShootLogoWrapperClass}>
+                            <a href="/" class={satShootLogoClass}>
+                                <img
+                                    src="/img/satshoot.svg"
+                                    alt="Satshoot logo"
+                                    class="w-full max-w-[45px]"
                                 />
-                            </button>
-                        {:else if $loggingIn}
-                            <div class="flex items-center gap-x-2">
-                                <h3 class="h6 md:h3 font-bold">Logging in...</h3>
-                                <ProgressRing color="primary" size={12} />
+                                <p class="max-[576px]:hidden">SatShoot</p>
+                            </a>
+                        </div>
+                        <div class="flex flex-row grow gap-4 justify-end items-center">
+                            {#if $loggedIn}
+                                <!-- search input in desktop view -->
+                                <div class="relative hidden sm:block">
+                                    <Input
+                                        bind:value={searchInput}
+                                        onKeyPress={handleSearch}
+                                        placeholder="search"
+                                    />
+                                    {#if searchInput.length}
+                                        <Button
+                                            variant="text"
+                                            onClick={clearSearch}
+                                            classes="absolute top-0 bottom-0 right-0"
+                                        >
+                                            <i class="bx bx-x"></i>
+                                        </Button>
+                                    {/if}
+                                </div>
+
+                                <!-- a trigger for opening search input in mobile view -->
+                                <div class="sm:hidden">
+                                    <Button
+                                        variant="outlined"
+                                        onClick={() => (displaySearchInput = true)}
+                                    >
+                                        <i class="bx bx-search text-xl"></i>
+                                    </Button>
+                                </div>
+
+                                <Button href="/post-job/">Submit Job</Button>
+
+                                <!-- User Avatar and trigger for user app menu  -->
+                                <button onclick={() => (showAppMenu = !showAppMenu)}>
+                                    <!-- Avatar image -->
+                                    <Avatar
+                                        classes="rounded-full border-white placeholder-white cursor-pointer"
+                                        size="size-14"
+                                        border="border-4 border-surface-300-600 hover:border-primary-500!"
+                                        src={profilePicture ??
+                                            getRoboHashPicture($currentUser?.pubkey ?? '')}
+                                        name={$currentUser?.profile?.displayName ?? ''}
+                                    />
+                                </button>
+                            {:else if $loggingIn}
+                                <div class="flex items-center gap-x-2">
+                                    <h3 class="h6 md:h3 font-bold">Logging in...</h3>
+                                    <ProgressRing color="primary" size={12} />
+                                </div>
+                            {:else if $loginMethod === 'local'}
+                                <Button onClick={onRestoreLogin}>Login</Button>
+                            {:else}
+                                <Button onClick={handleLogin}>Login</Button>
+                            {/if}
+                        </div>
+                    {:else}
+                        <!-- In mobile view when displaySearchInput is true, replace normal header with search input -->
+                        <div
+                            class="flex flex-row grow gap-4 items-center justify-evenly sm:hidden py-[10px]"
+                        >
+                            <Button variant="outlined" onClick={() => (displaySearchInput = false)}>
+                                <i class="bx bx-chevron-left text-xl"></i>
+                            </Button>
+                            <div class="relative">
+                                <Input
+                                    bind:value={searchInput}
+                                    onKeyPress={handleSearch}
+                                    placeholder="search"
+                                />
+                                {#if searchInput.length}
+                                    <Button
+                                        variant="text"
+                                        onClick={clearSearch}
+                                        classes="absolute top-0 bottom-0 right-0"
+                                    >
+                                        <i class="bx bx-x"></i>
+                                    </Button>
+                                {/if}
                             </div>
-                        {:else if $loginMethod === 'local'}
-                            <Button onClick={onRestoreLogin}>Login</Button>
-                        {:else}
-                            <Button onClick={handleLogin}>Login</Button>
-                        {/if}
-                    </div>
+                            <i class="bx bx-search text-2xl mt-[5px]"></i>
+                        </div>
+                    {/if}
                 </div>
             </div>
         </div>
