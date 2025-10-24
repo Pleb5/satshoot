2bd067b501caa78c738354ee8384c94096d14355
Repository Maintainer <maintainer@nostr-git.local>
2025-10-24 12:33:42 +0200

    Merge patch: Improve the titles of the mnemonic modals

---
+++ apps/satshoot/src/lib/components/Modals/MnemonicSeedGenerationModal.svelte
@@ -0,0 +1,7 @@
+    }
+</script>
+
+<ModalWrapper bind:isOpen title="Mnemonic Seed Words for Nostr Cashu Wallet">
+    {#if step === 0}
+        <section class="flex flex-col gap-4">
+            <p>
---
+++ apps/satshoot/src/lib/components/Modals/MnemonicSeedInputModal.svelte
@@ -0,0 +1,8 @@
+    }
+</script>
+
+<ModalWrapper bind:isOpen title="Input Seed Words of your Nostr Cashu Wallet">
+    <section class="flex flex-col gap-4">
+        <span>
+            If you already have a Nostr Cashu wallet backed up with mnemonic seed words (NUT-13),
+libgit2 1.9.0
