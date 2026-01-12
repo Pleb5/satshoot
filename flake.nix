e45904080da84a15ca6ff18e8e37dc9dee586f29
Five <co22rkn@protonmail.com>
2026-01-12 12:15:12 +0100

    Chore: Remove nixvim from devshell flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
 description = ''A flake that creates a devShell containing the following:
-		- Nixvim (based on nixos-unstable)
 		- NodeJS
 		- PNPM
 		- Turbo
@@ -10,19 +9,16 @@ description = ''A flake that creates a devShell containing the following:
 inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    nixvim.url = "github:Pleb5/neovim-flake/master";
 };
 
-outputs = { self, nixpkgs, flake-utils, nixvim, ... }:
+outputs = { self, nixpkgs, flake-utils, ... }:
 
     flake-utils.lib.eachDefaultSystem (system:
         let
             pkgs = nixpkgs.legacyPackages.${system};
-            nvim = nixvim.packages.${system}.nvim;
         in {
             devShell = pkgs.mkShell {
                 buildInputs = [ 
-                    nvim
                     pkgs.ripgrep
                     pkgs.nodejs_20 
                     pkgs.nodePackages.pnpm 
